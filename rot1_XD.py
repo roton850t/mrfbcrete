9e78121a054d9dfd3281215752b2d1fd95906d23
roton850t <marknaog@gmail.com>
2026-03-01 10:00:58 +0600

    Update rot1_XD.py

--- rot1_XD.py
+++ rot1_XD.py
@@ -3,8 +3,8 @@ import sys
 
 def main():
     if platform.architecture()[0] == "64bit":
-        import stex_XD
-        stex_XD.asyncio.run()
+        import rot1_XD
+        rot1_XD.asyncio.run()
     else:
         print("32bit Not Supported! Sorry")
         sys.exit(1)
