695ad37d7b9b83829fba6b186eba019bc97ba70c
roton850t <marknaog@gmail.com>
2026-03-01 09:51:18 +0600

    Add architecture check in rot1_XD.py

---
+++ rot1_XD.py
@@ -0,0 +1,10 @@
+import platform
+import sys
+
+def main():
+    if platform.architecture()[0] == "64bit":
+        import stex_XD
+        stex_XD.approval()
+    else:
+        print("32bit Not Supported! Sorry")
+        sys.exit(1)
