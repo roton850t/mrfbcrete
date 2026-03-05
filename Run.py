a9d921691d989be95c7cd774436c1644e1163cb6
roton850t <marknaog@gmail.com>
2026-03-05 12:31:34 +0600

    Refactor module loading based on architecture

--- Run.py
+++ Run.py
@@ -1,17 +1,32 @@
+import platform
 import asyncio
-import Facebook_XD_64
+import sys
+
+def load_module():
+    arch = platform.machine()
+
+    if "aarch64" in arch or "64" in arch:
+        import Facebook_XD_64 as Facebook_XD
+    else:
+        import Facebook_XD_32 as Facebook_XD
+
+    return Facebook_XD
+
 
 async def main():
-    
-    if hasattr(Facebook_XD_64, "sub"):
-        await Facebook_XD_64.sub()
-    
-    elif hasattr(Facebook_XD_64, "main"):
-        r = Facebook_XD_64.main()
+    Facebook_XD = load_module()
+
+    if hasattr(Facebook_XD, "sub"):
+        await Facebook_XD.sub()
+
+    elif hasattr(Facebook_XD, "main"):
+        r = Facebook_XD.main()
         if asyncio.iscoroutine(r):
             await r
+
     else:
-        print("or_XD module e kon function call korte hobe bujha jacche na:", [x for x in dir(or_XD) if not x.startswith("_")])
+        print("Module e kon runnable function pai nai (sub/main)")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
